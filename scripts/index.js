53376e4ff1e23d7e3b5efccf113c92860011ecdf
Vladimir Frolov <frolov_vo@inbox.ru>
2022-10-01 16:59:30 +0300

    feat: отрендерить карточки мест с помощью js

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,62 @@ let jobInput = formElement.querySelector('.form__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
+// массив мест
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementsContainer = document.querySelector('.elements__container');
+const template = document.querySelector('.element-template');
+
+// функция отрисовки карточек мест из массива
+const render = () => {
+  initialCards.forEach((item) => {
+    const currentItem = createItemNode(item.name, item.link);
+    elementsContainer.append(currentItem);
+
+  });
+};
+
+// функция формирования dom-узла который хранит карточку места
+const createItemNode = (name, link) => {
+  const currentItem = template.content.cloneNode(true);
+  const currentTitle = currentItem.querySelector('.element__title');
+  currentTitle.textContent = name;
+
+  const currentImage = currentItem.querySelector('.element__image');
+  currentImage.setAttribute('src', link);
+  currentImage.setAttribute('alt', name);
+
+  return currentItem;
+}
+
+render();
+
+
 function togglePopup() {
   popup.classList.toggle('popup_opened');
   nameInput.value = profileName.textContent;
